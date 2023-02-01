cce6cad37f1f3234539c03f2613433bcf9075caa
olivia <opierez@gmail.com>
2023-01-31 20:07:21 -0500

    commit tree 59bde883786bb1e1cb40ebfe99ec09f6fe9d99a6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"] }
+    validate :valid_title?
+
+    def valid_title?
+        unless title =~ /Won\'t Believe|Secret|Top \d+|Guess/
+            errors.add(:title, "must include one of the following phrases: 'Won't Believe', 'Secret', 'Top [number]', 'Guess'")
+        end
+    end
+
+
+
 end
